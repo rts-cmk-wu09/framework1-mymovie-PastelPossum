27a99a559bbf1bd6be6415bbcf5d9949f7b926ca
PastelPossum <iggyskywalker@gmail.com>
2023-04-20 11:20:09 +0200

    add moviecard component

---
+++ src/templates/MovieCard.jsx
@@ -0,0 +1,14 @@
+import Image from "../components/Image"
+
+//SFC
+const MovieCard = () => {
+  return (
+    <article>
+      <figure>
+        <Image />
+      </figure>
+    </article>
+  );
+};
+
+export default MovieCard;
