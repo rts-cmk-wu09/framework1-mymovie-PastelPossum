caaa017b9dd9290539cc6693ec3082a5efa036db
PastelPossum <iggyskywalker@gmail.com>
2023-04-20 11:18:53 +0200

    Add heading and moviecard to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import './App.css'
+import "./App.css";
+import HeadingOne from "./components/headingOne";
+import MovieCard from "./templates/MovieCard";
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <HeadingOne />
+      </header>
+      <main>
+        <section>
+          <MovieCard />
+        </section>
+        <section></section>
+      </main>
+      <footer>
+        <nav></nav>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
